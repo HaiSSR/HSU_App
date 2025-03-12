8c04585a726d8a4b1f85408858700cb0efaac1b3
HaiSSR <94343525+HaiSSR@users.noreply.github.com>
2025-03-13 04:35:30 +0700

    Initial commit

---
+++ HSU_App/View/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Maui.Controls;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace HSU_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnTogglePasswordClicked(object sender, EventArgs e)
+        {
+            passwordEntry.IsPassword = !passwordEntry.IsPassword;
+
+            // Optionally, toggle the icon image
+            /*
+             * if (passwordEntry.IsPassword)
+            {
+                togglePasswordButton.Source = "eye_on";
+            }
+            else
+            {
+                togglePasswordButton.Source = "eye_off";
+            }
+            */
+        }
+
+    }
+}
+
---
+++ HSU_App/ViewModel/LoginViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Windows.Input;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HSU_App.ViewModels
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string? usernameEntry;
+
+        [ObservableProperty]
+        private string? passwordEntry;
+
+        public ICommand LoginCommand { get; }
+        public ICommand ForgotPasswordCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+            ForgotPasswordCommand = new RelayCommand(ForgotPassword);
+        }
+
+        private async void Login()
+        {
+            if (IsCredentialCorrect(usernameEntry, passwordEntry))
+            {
+                await Application.Current.MainPage.DisplayAlert("Đăng Nhập Thành Công", "", "OK");
+                //await Application.Current.MainPage.Navigation.PushAsync(new HomePage());
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Sai mật khẩu", "Mật khẩu của bạn vừa nhập chưa\r\nchính xác. Vui lòng thử lại.", "OK");
+            }
+        }
+
+        private static bool IsCredentialCorrect(string username, string password)
+        {
+            //Authen logic 
+
+            //hardcoded login
+            return username == "asd" && password == "asd";
+        }
+
+        private async void ForgotPassword()
+        {
+            await Application.Current.MainPage.DisplayAlert("Not yet", "To be implemented", "OK");
+            //await Application.Current.MainPage.Navigation.PushAsync(new ForgotPasswordPage());
+        }
+    }
+
+}
